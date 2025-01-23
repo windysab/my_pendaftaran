44560c1184d976f358694dba31171960a959a650
windysab <windysabtami85@gmail.com>
2025-01-23 16:43:39 +0800

    dfd

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 
 Route::get('/gugatan/create', function () {
     return view('gugatan.create');
-});
+})->name('gugatan.create');
 
 Route::get('/layanan', function () {
     return view('layanan');
