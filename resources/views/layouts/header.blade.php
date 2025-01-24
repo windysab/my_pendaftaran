63563d5f3769406985470baaabb48eaf930d3c33
windysab <windysabtami85@gmail.com>
2025-01-24 09:32:20 +0800

    oke lanjut

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,6 +2,7 @@
     <div class="container mx-auto flex justify-between items-center px-6">
         <!-- Logo Section -->
         <a href="/" class="flex items-center space-x-3">
+            <img src="/path/to/logo.png" alt="Logo" class="h-8 w-8"> <!-- Add logo here -->
             <span class="text-white text-xl font-bold tracking-wide">Pengadilan Agama Amuntai</span>
         </a>
 
@@ -23,7 +24,7 @@
                         Daftar Gugatan
                     </a>
                 </li>
-                
+
                 <li>
                     <a href="/sidang" class="text-white text-lg font-medium hover:text-yellow-300 transition duration-300 relative">
                         Daftar Sidang
