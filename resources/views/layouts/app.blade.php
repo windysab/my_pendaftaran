3492f029f37a87124fbd11bc441463d7a2de8f5f
windysab <windysabtami85@gmail.com>
2025-01-24 10:15:32 +0800

    okes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,17 @@
         <div class="container mx-auto px-6">
             <div class="text-center">
                 <p>&copy; {{ date('Y') }} Pengadilan Agama Amuntai. All rights reserved.</p>
+                <div class="mt-4 flex justify-center">
+                    <a href="https://facebook.com" class="text-white mx-2">
+                        <img src="/path/to/facebook-logo.png" alt="Facebook" class="inline-block h-6">
+                    </a>
+                    <a href="https://twitter.com" class="text-white mx-2">
+                        <img src="/path/to/twitter-logo.png" alt="Twitter" class="inline-block h-6">
+                    </a>
+                    <a href="https://instagram.com" class="text-white mx-2">
+                        <img src="/path/to/instagram-logo.png" alt="Instagram" class="inline-block h-6">
+                    </a>
+                </div>
             </div>
         </div>
     </footer>
