2bef7442488117c2ad0d67cfb2d12e6c893a50fe
Windy Sabtami AJ <windysabtami85@gmail.com>
2025-01-25 21:02:47 +0800

    okg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        './node_modules/flyonui/dist/js/*.js',
     ],
     theme: {
-      extend: {
-        colors: {
-          'court-green': {
-            50: '#f0fdf4',
-            100: '#dcfce7',
-            500: '#22c55e',
-            600: '#16a34a',
-            700: '#15803d',
-            800: '#166534',
-            900: '#14532d',
-          }
+        extend: {
+            colors: {
+                'court-green': {
+                    50: '#f0fdf4',
+                    100: '#dcfce7',
+                    500: '#22c55e',
+                    600: '#16a34a',
+                    700: '#15803d',
+                    800: '#166534',
+                    900: '#14532d',
+                }
+            },
+            fontFamily: {
+                'inter': ['Inter', 'sans-serif'],
+            },
         },
-        fontFamily: {
-          'inter': ['Inter', 'sans-serif'],
-        },
-      },
     },
     plugins: [
-        require('flyonui')
+        require('flyonui'),
+        require('flyonui/plugin')
     ],
-  }
+}
