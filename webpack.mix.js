cc96f245081942056f3336d942d327d3aae94d1f
Windy Sabtami AJ <windysabtami85@gmail.com>
2025-05-12 22:10:17 +0800

    sjs

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,17 @@
 const mix = require('laravel-mix');
-const path = require('path');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .postCss('resources/css/app.css', 'public/css', [
-       require('tailwindcss'),
-   ])
-   .webpackConfig({
-       resolve: {
-           alias: {
-               flyonui: path.resolve(__dirname, 'node_modules/flyonui'),
-           },
-       },
-   });
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
